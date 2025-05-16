2a0a77537bd0717e201de213a4cab96b547941b9
saudlari <larissa@riseup.net>
2025-05-16 10:48:04 +0200

    feat(User) : create user model with lombok and validations

---
+++ src/main/java/com/rizoma/rizoma/model/User.java
@@ -0,0 +1,51 @@
+package com.rizoma.rizoma.model;
+
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;  
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @SequenceGenerator(name = "user_id_sequence", sequenceName = "user_id_sequence", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_sequence")
+    private Integer id;
+    
+    @Column
+    @NotBlank(message = "[ERROR!] Name camp is required and cannot be empty")
+    @Pattern(regexp = "^[^\\/:*?\\\"<>|]+$", message = "[ERROR!] No está permitido el uso de caracteres especiales")
+    @Size(max = 20, message = "[ERROR!] Maximum of 20 characters allowed in this field")
+    private String username;
+
+    @Column
+    @NotBlank(message = "[ERROR!] Password camp is required and cannot be empty")    
+    @Size(max = 20, message = "[ERROR!] Maximum of 20 characters allowed in this field")
+    private String password;
+
+    @Column
+    @Email(message = "[ERROR!] Please enter a valid email format")
+    @NotBlank(message = "[ERROR!] Email camp is required and cannot be empty")
+    @Size(max = 50, message = "[ERROR!] Maximum of 50 characters allowed in this field")
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Marks> marks;
+}
