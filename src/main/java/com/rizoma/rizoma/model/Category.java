13b07cca5d614e45344b96ea363adc9c657b0e1b
saudlari <larissa@riseup.net>
2025-05-18 18:33:39 +0200

    feat (Category) : create model category with lombok and jakarta persistence

---
+++ src/main/java/com/rizoma/rizoma/model/Category.java
@@ -0,0 +1,27 @@
+package com.rizoma.rizoma.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Category {
+    @Id
+    @SequenceGenerator(name = "category_id_sequence", sequenceName = "category_id_sequence", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_id_sequence")
+    private Integer id;
+
+    @Column
+    private String nameCategory;
+}
